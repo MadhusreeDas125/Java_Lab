4492f444a3fa56ff262edd73bdc1948ba8d52a47
Madhusree125 <madhusree.das@s.amity.edu>
2026-03-13 15:31:44 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class Exception {
+    public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("first no:");
+        int a=scanner.nextInt();
+        System.out.println("second no:");
+        int b=scanner.nextInt();
+        try{
+            int result=a/b;
+            System.out.println("result: "+result);
+        }
+        catch(ArithmeticException e){
+            System.out.println("ERROR!,CAN NOT DEVIDE BY ZERO");
+        }
+        scanner.close();
+
+    }
+
+    
+}
